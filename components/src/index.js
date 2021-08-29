da919d3a0c2204710cff2149a6eadf77fe245e11
Darrick Smith <smithwebdd@gmail.com>
2021-08-29 02:15:23 -0400

    raw component duplicated

--- components/src/index.js
+++ components/src/index.js
@@ -17,6 +17,30 @@ const App = () => {
           <div className="text">Nice Blog post!</div>
         </div>
       </div>
+      <div className="comment">
+        <a className="avatar" href="/"><img alt="avatar" src={faker.image.avatar()}/></a>
+        <div className="content">
+          <a className="author" href="/">
+            Adam
+          </a>
+          <div className="metadata">
+            <span className="date">Today at 6:00pm</span>
+          </div>
+          <div className="text">Nice Blog post!</div>
+        </div>
+      </div>
+      <div className="comment">
+        <a className="avatar" href="/"><img alt="avatar" src={faker.image.avatar()}/></a>
+        <div className="content">
+          <a className="author" href="/">
+            Matt
+          </a>
+          <div className="metadata">
+            <span className="date">Today at 6:00pm</span>
+          </div>
+          <div className="text">Nice Blog post!</div>
+        </div>
+      </div>
     </div>
   )
 }
