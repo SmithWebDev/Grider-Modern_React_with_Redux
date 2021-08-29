b2b1b7c0f1f1358f89e5c44a7c96b75df6018cc0
Darrick Smith <smithwebdd@gmail.com>
2021-08-29 11:33:12 -0400

    cleaned CommentDetail, wrapped CommentDetail in Approval Card

--- components/src/index.js
+++ components/src/index.js
@@ -2,13 +2,35 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import faker from 'faker';
 import CommentDetail from './CommentDetail';
+import ApprovalCard from './ApprovalCard';
 
 const App = () => {
   return (
-    <div className ='ui container comments'>
-      <CommentDetail author="Mike" timeAgo="Yesterday at 9:50pm" avatarImage={faker.image.avatar()} commentText="Nice Blog post!"/>
-      <CommentDetail author="Jane" timeAgo="Yesterday at 9:00pm" avatarImage={faker.image.avatar()} commentText="Nice Blog post!"/>
-      <CommentDetail author="Alex" timeAgo="Yesterday at 7:40pm" avatarImage={faker.image.avatar()} commentText="Nice Blog post!"/>
+    <div className='ui container comments'>
+      <ApprovalCard>
+        <CommentDetail
+          author="Mike"
+          timeAgo="Yesterday at 9:50pm"
+          avatarImage={faker.image.avatar()}
+          commentText="Nice Blog post!"
+        />
+      </ApprovalCard>
+      <ApprovalCard>
+        <CommentDetail 
+          author="Jane" 
+          timeAgo="Yesterday at 9:00pm" 
+          avatarImage={faker.image.avatar()} 
+          commentText="Nice Blog post!" 
+        />
+      </ApprovalCard>
+      <ApprovalCard>
+        <CommentDetail 
+          author="Alex" 
+          timeAgo="Yesterday at 7:40pm" 
+          avatarImage={faker.image.avatar()} 
+          commentText="Nice Blog post!" 
+        />
+      </ApprovalCard>
     </div>
   )
 }
