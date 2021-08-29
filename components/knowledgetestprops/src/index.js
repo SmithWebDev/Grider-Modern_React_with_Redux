65a24e8c224ddb39644c7da09f6f1bc732749963
Darrick Smith <smithwebdd@gmail.com>
2021-08-29 13:10:11 -0400

    completed Props exercise for components section

---
+++ components/knowledgetestprops/src/index.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+const App = () => {
+  return (
+    <div>
+      <Message 
+        header='Changes in Service'
+        text='We just updated our privacy policy here to better service our customers.'
+      />
+    </div>
+  );
+};
+
+const Message = (props) => {
+  return (
+    <div className="ui message">
+      <div className="header">{props.header}</div>
+      <p>{props.text}</p>
+    </div>
+  );
+};
+ReactDOM.render(<App />, document.querySelector('#root'));
