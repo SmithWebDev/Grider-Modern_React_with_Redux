c0fb4058c48761404720ae2fe83ef2acd6069b9f
Darrick Smith <smithwebdd@gmail.com>
2021-08-29 01:46:20 -0400

    initial comment created

--- components/src/index.js
+++ components/src/index.js
@@ -3,7 +3,20 @@ import ReactDOM from 'react-dom'
 
 const App = () => {
   return (
-    <div>Hi There</div>
+    <div className ='ui container comments'>
+      <div className="comment">
+        <a className="avatar" href="/"><img alt="avatar" /></a>
+        <div className="content">
+          <a className="author" href="/">
+            Sam
+          </a>
+          <div className="metadata">
+            <span className="date">Today at 6:00pm</span>
+          </div>
+          <div className="text">Nice Blog post!</div>
+        </div>
+      </div>
+    </div>
   )
 }
 
