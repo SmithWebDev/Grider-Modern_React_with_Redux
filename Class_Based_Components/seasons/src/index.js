74d037e16bfd943b67e39d2b06b158acd4155453
Darrick Smith <smithwebdd@gmail.com>
2021-09-01 01:09:32 -0400

    defined constructor method, set initial state for class based component

--- Class_Based_Components/seasons/src/index.js
+++ Class_Based_Components/seasons/src/index.js
@@ -2,6 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 
 //Functional based component
+//
 //const App = () => {
 //  window.navigator.geolocation.getCurrentPosition(
 //    (position) => console.log(position),
@@ -12,8 +13,18 @@ import ReactDOM from 'react-dom'
 //  return <div>Latitude:</div>
 //};
 
+
 //Class Based Component
 class App extends React.Component {
+  //Any new instance is created, the constructor is the first item called.
+  //Therefore a good option to initialize state.
+  constructor(props) {
+    super(props);
+
+    this.state = { lat: null };
+  }
+
+  //Class based components MUST define render
   render() {
     window.navigator.geolocation.getCurrentPosition(
       (position) => console.log(position),
@@ -25,3 +36,11 @@ class App extends React.Component {
 
 
 ReactDOM.render(<App />, document.querySelector('#root'));
+
+//NOTES
+//State - only usable with class based components
+//State - a JS object that contains data relevant to a component
+//NOTE: it is easy to confuse props with state, be mindful of this
+//Updating 'state' on a component causes the component to (almost) instantly render
+//State - must be initialized when a component is created
+//State - can only be updated using the function 'setState'
