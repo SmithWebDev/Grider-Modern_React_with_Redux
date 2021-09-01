e04cbb3ce65ae3fe94eab98a83078e7544d72b91
Darrick Smith <smithwebdd@gmail.com>
2021-09-01 02:39:07 -0400

    cleaned up workspace, next lesson will introduce Component Lifecycle Methods

--- Class_Based_Components/seasons/src/index.js
+++ Class_Based_Components/seasons/src/index.js
@@ -1,29 +1,54 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
-//Functional based component
+
+// NOTES
+// State - only usable with class based components
+// State - a JS object that contains data relevant to a component
+// NOTE: it is easy to confuse props with state, be mindful of this
+// Updating 'state' on a component causes the component to (almost) instantly render
+// State - must be initialized when a component is created
+// State - can only be updated using the function 'setState'
+
+// Lifecycle Methods
+// componentDidMount - Content visible on screen
+// componentDidUpdate - Sit and wait for updates
+// componentWillUnmount - Sit and wait until this component is no longer shown
+
+
+//Class Based Component
+//class App extends React.Component {
+//  //Any new instance is created, the constructor is the first item called.
+//  //Therefore a good option to initialize state.
+//  constructor(props) {
+//    super(props);
 //
-//const App = () => {
-//  window.navigator.geolocation.getCurrentPosition(
-//    (position) => console.log(position),
-//    (err) => console.log(err)
-//  );
+//    this.state = { lat: null, errorMessage: '' };
 //
+//    window.navigator.geolocation.getCurrentPosition(
+//      (position) => {
+//        // Call setState to update the state information. functions should not
+//        // be found in the render() portion of the class.
+//        this.setState({ lat: position.coords.latitude });
+//      },
+//      (err) => {
+//        this.setState({ errorMessage: err.message });
+//      }
 //
-//  return <div>Latitude:</div>
-//};
-
-
-//NOTES
-//State - only usable with class based components
-//State - a JS object that contains data relevant to a component
-//NOTE: it is easy to confuse props with state, be mindful of this
-//Updating 'state' on a component causes the component to (almost) instantly render
-//State - must be initialized when a component is created
-//State - can only be updated using the function 'setState'
-
-
-
+//    );
+//  }
+//
+//  //Class based components MUST define render
+//  render() {
+//    if (this.state.errorMessage && !this.state.lat) {
+//      return <div>Error: {this.state.errorMessage} </div>
+//    }
+//    if (this.state.lat && !this.state.errorMessage) {
+//      return <div> Latitude: {this.state.lat} </div>
+//    }
+//    return <div>Loading...</div>
+//  }
+//}
 
 //Class Based Component
 class App extends React.Component {
@@ -57,15 +82,7 @@ class App extends React.Component {
     }
     return <div>Loading...</div>
   }
-  //return (
-  //  <div>
-  //    Latitude: {this.state.lat} 
-  //    <br />
-  //    Error: {this.state.errorMessage}
-  //  </div>
-  //);
 }
-
 
 
 ReactDOM.render(<App />, document.querySelector('#root'));
