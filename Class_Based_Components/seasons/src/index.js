db282fd6cc48d92c743a6b3e5c278d62481ade81
Darrick Smith <smithwebdd@gmail.com>
2021-09-01 01:28:46 -0400

    removed function from render, placed logic in the constructor, used setState to update information rendered on the client

--- Class_Based_Components/seasons/src/index.js
+++ Class_Based_Components/seasons/src/index.js
@@ -14,6 +14,17 @@ import ReactDOM from 'react-dom'
 //};
 
 
+//NOTES
+//State - only usable with class based components
+//State - a JS object that contains data relevant to a component
+//NOTE: it is easy to confuse props with state, be mindful of this
+//Updating 'state' on a component causes the component to (almost) instantly render
+//State - must be initialized when a component is created
+//State - can only be updated using the function 'setState'
+
+
+
+
 //Class Based Component
 class App extends React.Component {
   //Any new instance is created, the constructor is the first item called.
@@ -22,25 +33,24 @@ class App extends React.Component {
     super(props);
 
     this.state = { lat: null };
+
+    window.navigator.geolocation.getCurrentPosition(
+      (position) => {
+
+        // Call setState to update the state information. functions should not
+        // be found in the render() portion of the class.
+        this.setState({ lat: position.coords.latitude });
+      },
+      (err) => console.log(err)
+    );
   }
 
   //Class based components MUST define render
   render() {
-    window.navigator.geolocation.getCurrentPosition(
-      (position) => console.log(position),
-      (err) => console.log(err)
-    );
-    return <div>Latitude:</div>;
+    return <div>Latitude: {this.state.lat} </div>;
   }
 }
 
 
 ReactDOM.render(<App />, document.querySelector('#root'));
 
-//NOTES
-//State - only usable with class based components
-//State - a JS object that contains data relevant to a component
-//NOTE: it is easy to confuse props with state, be mindful of this
-//Updating 'state' on a component causes the component to (almost) instantly render
-//State - must be initialized when a component is created
-//State - can only be updated using the function 'setState'
